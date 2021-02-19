78595fd7aa17593f01caa64a595342b2af0bba84
Joy Kirii <joykirii@gmail.com>
2021-02-19 17:25:56 +0300

    added arrays for days of the week, male and female akan names

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+var femaleAkanNames = [
+    "Esi",
+    "Adjoa",
+    "Abena",
+    "Ekua",
+    "Aba",
+    "Efua",
+    "Amba",
+]
+
+var maleAkanNames = [
+    "Kwesi",
+    "Kojo",
+    "Kobeno",
+    "Kweku",
+    "Ekow",
+    "Kofi",
+    "Kwame",
+]
