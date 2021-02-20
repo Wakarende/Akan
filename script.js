f136646889a54a5e954d5d6bcb9b32e14f844934
Joy Kirii <joykirii@gmail.com>
2021-02-20 12:38:23 +0300

    link JS file to Html doc

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 
-var daysOfTheWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-]
+ let daysOfTheWeek = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday","Saturday"]
 
-var femaleAkanNames = [
-    "Esi",
-    "Adjoa",
-    "Abena",
-    "Ekua",
-    "Aba",
-    "Efua",
-    "Amba",
-]
+//  let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
-var maleAkanNames = [
-    "Kwesi",
-    "Kojo",
-    "Kobeno",
-    "Kweku",
-    "Ekow",
-    "Kofi",
-    "Kwame",
-]
+// let maleAkanNames = [ "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",]
+
+// let gender = document.getElementById
+// function  getGender() {
+
+// }
+//function getName(event) {
+//     event.preventDefault()
+//     late date =getElementById('date').value
+//     let myBrithday = new Date(date).toDateString()
+//     let day = myBrithday.split('')
+// }
+
+
+
+
+function getDay(event) {
+    event.preventDefault()
+    let date = document.getElementById('date').value
+    let myBirthday = new Date(date)
+    let message = '$(myBirthday)'
+    outputTag.innerHTML = message
+}
