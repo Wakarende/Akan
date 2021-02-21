368ab5f5425f6bfed460f56106c2efb581b47f0f
Joy Kirii <joykirii@gmail.com>
2021-02-21 19:40:04 +0300

    added function to display results on HTML

--- script.js
+++ script.js
@@ -18,32 +18,17 @@ const maleNames = [ "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",]
 
 
 if ( month < 1 || month > 12) {
-  alert("input valid month");
+  alert("input valid month e.g 12");
 } else if (day < 1 || day > 31) {
-  alert("input valid day")
+  alert("input valid day e.g")
 }else if (male.checked){
-("You were born on " + days[dayOfWeek] + "you are " + maleNames[dayOfWeek])
-// } else if ((male.checked) && (dayOfWeek == 0)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[0]);
-//   }else if ((male.checked) && (dayOfWeek == 1)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[1]);
-//   } else if ((male.checked) && (dayOfWeek == 2)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[2]);
-//   } else if ((male.checked) && (dayOfWeek == 3)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[3]);
-//   } else if ((male.checked) && (dayOfWeek == 4)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[4]);
-//   } else if ((male.checked) && (dayOfWeek == 5)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[5]);
-//   } else if ((male.checked) && (dayOfWeek == 6)) {
-//     alert("You were born on " + days[dayOfWeek] + " you are  " + maleNames[6]);
+    document.getElementById("name").innerHTML = ("You were born on " + days[dayOfWeek] + " your name is " + maleNames[dayOfWeek]);
   } else if (female.checked) {
-      alert("You were born on " + days[dayOfWeek] + "you are " + femaleNames[dayOfWeek]);
+      document.getElementById("name").innerHTML = ("You were born on " + days[dayOfWeek] + " your name is " + femaleNames[dayOfWeek]);
   } else {
    alert( "Kindly input your details correctly")
  }
  
 }
 
-// 
 
